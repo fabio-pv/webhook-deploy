a88c71aeaec839674b530537c6c801c171ab8e79
Fábio <fabiopv1994@gmail.com>
2021-03-03 11:56:28 -0300

    implementado filas para teste

---
+++ app/Jobs/ExecCommand.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class ExecCommand implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected string $command;
+
+    /**
+     * ExecCommand constructor.
+     * @param string $command
+     */
+    public function __construct(string $command)
+    {
+        $this->command = $command;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        exec($this->command, $result);
+    }
+}
